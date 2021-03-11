b9cc22c535b12d5d5dc2211d626b8994b034c9f9
Owen <os501517@ohio.edu>
2021-03-11 08:42:19 -0500

    Hello Git!! works @yhzhang1

---
+++ git-lab-program.cc
@@ -0,0 +1,20 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Owen Stratton
+ *        Date: 3/11/2021
+ * Description: Git Lab // lab#2 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+cout << "Hello Git!!" << endl;
+
+    /*add code*/
+    return 0;
+}// main
